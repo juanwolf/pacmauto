f60bf4bca011671786a7789aa4cd4d27d38dc11b
Jean-Loup Adde <jean-loup.adde@orange.fr>
2014-04-08 11:40:17 +0200

    Bug resolved. When the script is launched from a cron it didn't work.

--- pacmauto.sh
+++ pacmauto.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 while inotifywait -e create /var/lib/pacman; do
-	notify-send pacmauto 'Updating...';
+	 /usr/bin/notify-send pacmauto 'Updating...';
 	updateDone=0
 	while inotifywait -e delete /var/lib/pacman -t 30; do
 		updateDone=1;
 	done
 	if [ $updateDone -eq 1 ]; then
-		notify-send pacmauto "Update done, you can install it with pacman -Su";
+		 /usr/bin/notify-send pacmauto "Update done, you can install it with pacman -Su";
 	fi
 done
 
