bc1c52500cebdb8dcc3c5f5f9fac1372301de1ac
Jean-Loup Adde <jean-loup.adde@orange.fr>
2014-04-08 02:57:43 +0200

    Fixing issue.

--- pacmauto.sh
+++ pacmauto.sh
@@ -2,9 +2,13 @@
 
 while inotifywait -e create /var/lib/pacman; do
 	notify-send pacmauto 'Updating...';
-	while inotifywait -e delete /var/lib/pacman; do
-		notify-send pacmauto "Update done, you can install it with pacman -Su";
+	updateDone=0
+	while inotifywait -e delete /var/lib/pacman -t 30; do
+		updateDone=1;
 	done
+	if [ $updateDone -eq 1 ]; then
+		notify-send pacmauto "Update done, you can install it with pacman -Su";
+	fi
 done
 
 
