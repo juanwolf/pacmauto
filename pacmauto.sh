4897eaea98679adb23f156db98381c7f931f88d8
Jean-Loup Adde <jean-loup.adde@orange.fr>
2014-04-09 12:43:41 +0200

    Adding path for the inotifywait bin

--- pacmauto.sh
+++ pacmauto.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-while inotifywait -e create /var/lib/pacman; do
-	 /usr/bin/notify-send pacmauto 'Updating...';
+while /usr/bin/inotifywait -e create /var/lib/pacman; do
+	/usr/bin/notify-send "pacmauto" "Updating...";
 	updateDone=0
-	while inotifywait -e delete /var/lib/pacman -t 30; do
+	while /usr/bin/inotifywait -e delete /var/lib/pacman -t 120; do
 		updateDone=1;
 	done
 	if [ $updateDone -eq 1 ]; then
-		 /usr/bin/notify-send pacmauto "Update done, you can install it with pacman -Su";
+		/usr/bin/notify-send "pacmauto" "Update done, you can install it with pacman -Su";
 	fi
 done
 
